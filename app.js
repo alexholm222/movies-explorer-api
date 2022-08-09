df58cabc0e5be9760524a88c31df5ebc5431d23e
Aleksey Kholmogorov <alexholm222@yandex.ru>
2022-08-10 01:12:01 +0300

    fix: Исправление подключения

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const { errors } = require('celebrate');
 const helmet = require('helmet');
 const error = require('./middlewares/error');
 const cors = require('./middlewares/cors');
-const limiter = require('./middlewares/rateLimiter');
+const { limiter } = require('./middlewares/rateLimiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes/index');
-const { PORT } = require('./utils/config');
+const { MONGOD_SERVER, PORT } = require('./utils/config');
 const { SERVER_START } = require('./utils/constants');
 
 const app = express();
@@ -17,7 +17,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/moviesdb', {
+mongoose.connect(MONGOD_SERVER, {
   useNewUrlParser: true,
 });
 
