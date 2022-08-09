ec7422d3cab5d70822b979affd77806e29ecb790
Aleksey Kholmogorov <alexholm222@yandex.ru>
2022-08-10 01:42:24 +0300

    fix: исправление замечаний ревью

--- app.js
+++ app.js
@@ -6,25 +6,23 @@ const { errors } = require('celebrate');
 const helmet = require('helmet');
 const error = require('./middlewares/error');
 const cors = require('./middlewares/cors');
-/* const { limiter } = require('./middlewares/rateLimiter'); */
+const { limiter } = require('./middlewares/rateLimiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes/index');
-/* const { MONGOD_SERVER, PORT } = require('./utils/config'); */
+const { MONGOD_SERVER, PORT } = require('./utils/config');
 const { SERVER_START } = require('./utils/constants');
-
-const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/moviesdb', {
+mongoose.connect(MONGOD_SERVER, {
   useNewUrlParser: true,
 });
 
 app.use(requestLogger);
-/* app.use(limiter); */
+app.use(limiter);
 app.use(cors);
 app.use(helmet());
 
--- utils/config.js
+++ utils/config.js
@@ -3,7 +3,7 @@ const {
   PORT = 3000,
   NODE_ENV,
   JWT_SECRET,
-}
+} = process.env;
 
 module.exports = {
   MONGOD_SERVER, PORT, NODE_ENV, JWT_SECRET,
